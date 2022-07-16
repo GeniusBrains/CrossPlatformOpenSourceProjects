cc7e5de4c54d6863a7b3a55b39e07f3e70866a4a
williamvietnam <williamnb.vn@gmail.com>
2022-07-16 22:16:43 +0700

    initialize project

---
+++ flutter-collect/flutter_collect/android/app/src/main/kotlin/com/flutter/flutter_collect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.flutter_collect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter/flutter_community/android/app/src/main/kotlin/com/flutter/flutter_community/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutter.flutter_community
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
